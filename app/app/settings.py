ae2b80a62ce6b709f1d8d54e8d8985010021da55
Mabarik667f <jc.bob1412@gmail.com>
2024-12-01 22:05:51 +0300

    feat: create POSTMAN collection

--- app/app/settings.py
+++ app/app/settings.py
@@ -172,6 +172,9 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication"
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated"
     ]
 }
 
